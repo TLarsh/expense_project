d6acb40a06256a754feb08ca21b49c2ace964b85
TLarsh <adeyemisam2015@gmail.com>
2025-04-18 07:16:05 +0100

    added swager ui

--- expense/urls.py
+++ expense/urls.py
@@ -3,7 +3,27 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from expense.views.user import RegisterUserView, GoogleLoginView
 from expense.views.expense import ExpenseListView, ExpenseUpdateView, ExpenseDeleteView
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Expense API",
+      default_version='v1',
+      description="Multi-Tenant SaaS Expense Management API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
     path('api/register/', RegisterUserView.as_view(), name='register'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- expense_project/settings.py
+++ expense_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
 
     # custom settings
+    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
     'django_celery_beat',
